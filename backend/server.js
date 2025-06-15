da6c00ae258686b582781af3fbff3f9a12fc437c
zadokalake <alakezadok@yahoo.com>
2025-06-15 13:29:42 +0100

    Initial backend commit

--- backend/server.js
+++ backend/server.js
@@ -44,4 +44,6 @@ app.put('/api/tasks/:id', async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
